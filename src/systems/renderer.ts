f139fcbd1949810726bca06cd3ac6b52d889b15e
dingqiang <dingqiang@neotrident.com>
2023-05-19 11:17:11 +0800

    ✨feat: 添加坑锯齿功能

--- src/systems/renderer.ts
+++ src/systems/renderer.ts
@@ -1,7 +1,10 @@
 import { WebGLRenderer } from 'three';
 
 export function createRenderer(){
-  const renderer = new WebGLRenderer();
+  const renderer = new WebGLRenderer({
+    // 抗锯齿
+    antialias: true, 
+  });
   
   return renderer;
 }
